8c70f08dd3a09e4fcb789d9edd203a2bf5eb3f14
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-07 11:22:21 -0300

    feat: modify and create folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{height: "100%"}}>
+      <div style={{backgroundClip: "#fafafafa"}}>
+        <p>teste</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
