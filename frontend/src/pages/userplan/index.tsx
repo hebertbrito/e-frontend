5281c9bd034a4dc4d66ce631712c29ddff06fd4a
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-07 20:09:28 -0300

    refactor: page userplan

--- frontend/src/pages/userplan/index.tsx
+++ frontend/src/pages/userplan/index.tsx
@@ -1,9 +1,10 @@
 import react from 'react'
 import './styles.css'
+import { InputMine, InputRadio, ButtonPage, ButtonConfirm } from '../../components'
 export class UserPlan extends react.PureComponent {
 
     state = {
-        enddate: new Date()
+        registrationDate: new Date()
     }
 
     teste(value: any) {
@@ -12,29 +13,35 @@ export class UserPlan extends react.PureComponent {
 
     render() {
         return (
-            <div className="container-userplan">
+            <div className="container-userplan formalign">
                 <div className="form">
-                    <h4>
-                        Plano E-pharma Teste
-                    </h4>
-                    <div className="sub-form-1">
-
-                    </div>
-                    <div className="sub-form-2">
-                        <label>Duração do Plano - em anos*</label>
-                        <label>Vigência inicial hoje</label>
-                        <div className="sub-form-1">
-                            <div className="check-box">
-                                <label>1 Ano</label>
-                                <input type="radio" name="typedurationplan" id="1" value="1" />
-                            </div>
-                            <div className="check-box">
-                                <label>2 Anos</label>
-                                <input type="radio" name="typedurationplan" id="2" value="2" />
-                            </div>
+                    <div className="container-type-person">
+                        <div className="divradio">
+                            <label>Pessoa Fisica</label>
+                            <InputRadio name="typeuser" id="pf" value="´pf" />
+                        </div>
+                        <div>
+                            <label>Pessoa Juridica</label>
+                            <InputRadio name="typeuser" id="pj" value="pj" />
                         </div>
                     </div>
-
+                    <div className="contentinputs">
+                        <InputMine placeholder="Nome do Plano" maxLength={100}/>
+                        <InputMine placeholder="Nome do recebidor" maxLength={100} />
+                    </div>
+                    <div className="contentinputs">
+                        <InputMine placeholder="D. Inicial Vigência: DD/MM/YY" maxLength={10}/>
+                        <InputMine placeholder="D. Final Vigência: DD/MM/YY" maxLength={10} />
+                    </div>
+                    
+                    <div className="contentinputs informationData">
+                        <h4>Data de cadastro:</h4>
+                        <label style={{ paddingLeft: "1%", color: "#dd2c00" }}>{this.state.registrationDate.toLocaleDateString("pt-BR")}</label>
+                    </div>
+                </div>
+                <div className="contButton">
+                    <ButtonPage />
+                    <ButtonConfirm />
                 </div>
             </div>
         )
