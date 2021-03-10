bf867dd103b9cdd4bb3e0efbe734f01e88921e6c
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-10 01:10:22 -0300

    feat: create plan to user

--- src/pages/userplan/index.tsx
+++ src/pages/userplan/index.tsx
@@ -1,50 +1,73 @@
 import react, { useState } from 'react'
 import './styles.css'
 import { InputMine, InputRadio, ButtonPage, ButtonConfirm } from '../../components'
-export const UserPlan = react.memo(()=>{
+import { CreatePlan } from '../../services/api/CreatePlan'
+
+export const UserPlan = react.memo(() => {
     const [date, setDate] = useState(new Date)
-    const [NameUser, setNameUser] = useState<string>()
-    const [PlanName, setPlanName] = useState<string>()
-    const [StartDatePlan, setStartDatePlan] = useState<string>()
-    const [EndDatePlan, setEndDatePlan] = useState<string>()
-    const [RegisterDate, setRegisterDate] = useState<string>()
+    const [nameUser, setNameUser] = useState<string>()
+    const [planName, setPlanName] = useState<string>()
+    const [startDatePlan, setStartDatePlan] = useState<string>()
+    const [endDatePlan, setEndDatePlan] = useState<string>()
+    const [registerDate, setRegisterDate] = useState(new Date().toLocaleDateString("pt-BR"))
     const [TypeUser, setTypeUser] = useState<string>("1")
 
+    const data = async function () {
+        try {
+            if(validate()){
 
+                const body = {
+                     date, nameUser, planName, startDatePlan, endDatePlan, registerDate
+                }
 
+                await CreatePlan(body)
+                alert("Plano Criado com sucesso")
+            }
+        } catch (error) {
+            alert("Erro no servidor ou parametros obrigatórios não preenchidos")
+        }
+    }
 
-    return(
+    const validate = function () {
+
+        if (startDatePlan && startDatePlan) {
+            const newEndDate = new Date(startDatePlan?.toString())
+            const newStartDate = new Date(startDatePlan?.toString())
+
+            if (newEndDate < newStartDate) {
+                return false
+            }
+        }
+
+        if (date && nameUser && planName && startDatePlan && endDatePlan && registerDate) {
+            return true
+        }
+
+        return false
+    }
+
+    return (
         <div className="container-userplan formalign">
-                <div className="form">
-                    <div className="container-type-person">
-                        <div className="divradio">
-                            <label>Pessoa Fisica</label>
-                            <InputRadio name="typeuser" id="1" value="´pf" setValue={setTypeUser}/>
-                        </div>
-                        <div>
-                            <label>Pessoa Juridica</label>
-                            <InputRadio name="typeuser" id="2" value="pj" setValue={setTypeUser}/>
-                        </div>
-                    </div>
-                    <div className="contentinputs">
-                        <InputMine placeholder="Nome do Plano" maxLength={100} value={PlanName} setValue={setPlanName}/>
-                        <InputMine placeholder="Nome do recebidor" maxLength={100} value={NameUser} setValue={setNameUser}/>
-                    </div>
-                    <div className="contentinputs">
-                        <InputMine placeholder="D. Inicial Vigência: DD/MM/YY" maxLength={10} value={StartDatePlan} setValue={setStartDatePlan}/>
-                        <InputMine placeholder="D. Final Vigência: DD/MM/YY" maxLength={10} value={EndDatePlan} setValue={setEndDatePlan}/>
-                    </div>
-                    
-                    <div className="contentinputs informationData">
-                        <h4>Data de cadastro:</h4>
-                        <label style={{ paddingLeft: "1%", color: "#dd2c00" }}>{date.toLocaleDateString("pt-BR")}</label>
-                    </div>
+            <div className="form">
+                <div className="contentinputs">
+                    <InputMine placeholder="Nome do Plano" maxLength={100} value={planName} setValue={setPlanName} />
+                    <InputMine placeholder="Nome do recebidor" maxLength={100} value={nameUser} setValue={setNameUser} />
                 </div>
-                <div className="contButton">
-                    <ButtonPage path="/listuserplan" wherego="Listagem"/>
-                    <ButtonPage path="/user" wherego="Usuario"/>
-                    {/* <ButtonConfirm /> */}
+                <div className="contentinputs">
+                    <InputMine placeholder="D. Inicial Vigência: DD/MM/YY" maxLength={10} value={startDatePlan} setValue={setStartDatePlan} />
+                    <InputMine placeholder="D. Final Vigência: DD/MM/YY" maxLength={10} value={endDatePlan} setValue={setEndDatePlan} />
+                </div>
+
+                <div className="contentinputs informationData">
+                    <h4>Data de cadastro:</h4>
+                    <label style={{ paddingLeft: "1%", color: "#dd2c00" }}>{date.toLocaleDateString("pt-BR")}</label>
                 </div>
             </div>
+            <div className="contButton">
+                <ButtonPage path="/listuserplan" wherego="Listagem" />
+                <ButtonPage path="/user" wherego="Usuario" />
+                <ButtonConfirm onPress={()=> data()}/>
+            </div>
+        </div>
     )
 })
---
+++ src/services/api/CreatePlan/index.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import https from 'https';
+const URL_LOCAL = "https://localhost:49163/api/plan/create"
+export async function CreatePlan(body: any){
+    try {
+        const response = await axios.post(URL_LOCAL, body, {
+            httpsAgent: new https.Agent({
+                requestCert: false,
+                rejectUnauthorized: false
+            })
+        })
+    } catch (error) {
+        Promise.reject(error)
+    }
+}
